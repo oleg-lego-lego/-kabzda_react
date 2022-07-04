0a542093e59844ffec66e60fe8d37937d898a607
oleg ostrovski <oleg.ostrovski@gmail.com>
2022-07-04 13:59:03 +0300

    add components AccordionTitle, AccordionBody, AppTitle

--- src/App.tsx
+++ src/App.tsx
@@ -4,35 +4,58 @@ import './App.css';
 function App() {
     return (
         <div>
-            This is App
+            <AppTitle/>
             <Rating/>
             <Accordion/>
+            <Rating/>
         </div>
     );
 }
 
-function Rating() {
+const AppTitle = () => {
     return (
-        <div>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-        </div>
+        <>This is App</>
     )
 }
 
-function Star() {
+const Rating = () => {
+    return (
+        <>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+        </>
+    )
+}
+
+const Star = () => {
     return (
         <div>star</div>
     )
 }
 
-function Accordion() {
-    return(
+const Accordion = () => {
+    return (
+        <div>
+            <AccordionTitle/>
+            <AccordionBody/>
+        </div>
+    )
+}
+
+const AccordionTitle = () => {
+    return (
         <div>
             <h3>меню</h3>
+        </div>
+    )
+}
+
+const AccordionBody = () => {
+    return (
+        <div>
             <ul>
                 <li>1</li>
                 <li>2</li>
@@ -40,7 +63,6 @@ function Accordion() {
             </ul>
         </div>
     )
-
 }
 
 export default App;
