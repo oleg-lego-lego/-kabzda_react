387735d59f7b35d67cd8210d3a1e0316a9d9bdbf
oleg ostrovski <oleg.ostrovski@gmail.com>
2022-08-22 11:19:32 +0300

    add generateData in useState without useMemo

--- src/components/UseState/UseState.tsx
+++ src/components/UseState/UseState.tsx
@@ -1,12 +1,12 @@
-import React, {useMemo, useState} from 'react';
+import React, {useState} from 'react';
 
 const generateData = () => {
     return 124235345;
 }
 
 export const UseState = () => {
-    const initValue = useMemo(generateData, [])
-    const[counter, setCounter] = useState(initValue)
+    //const initValue = useMemo(generateData, [])
+    const[counter, setCounter] = useState(generateData)
 
     return (
         <div>
